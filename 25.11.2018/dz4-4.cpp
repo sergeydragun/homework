cd96a7e55e37d8cece3bfb19c6479e78d34c584f
sergeydragun <43043493+sergeydragun@users.noreply.github.com>
2018-11-25 19:21:17 +0300

    Update dz4-4.cpp

--- 25.11.2018/dz4-4.cpp
+++ 25.11.2018/dz4-4.cpp
@@ -9,14 +9,27 @@ int main()
 	int *b = new int[n + 1];
 	for (int i = 0; i < n + 1; i++)
 		a[i] = i;
-	for (int p = 2; p < n + 1; p++)
+	
+	int p = 2;
+	int finish = false;
+	int finish2 = false;
+	int j = 0;
+	while(!finish)
 	{
+		p++
 		if (a[p] != 0)
 		{
+			
 			cout << a[p] << endl;
 			for (int k = p * p; k < n + 1; k += p) //otmechaim vse sostavnie chisla a potom ih ubiraem
 				a[p] = a[k];
 			    a[p] = 0;
+			j++;
+			if(j==n)
+			{
+			finish = true;
+			}
 		}
+		
 	}
 }
